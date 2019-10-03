36ed25e30d49563ae773e2a7ff8e6ceff8a976b2
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-10-03 15:00:42 -0400

    Hard Coded some Controller values

--- WebAPIDemo2/Controllers/MemoController.cs
+++ WebAPIDemo2/Controllers/MemoController.cs
@@ -4,21 +4,24 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebAPIDemo2.Models;
 
 namespace WebAPIDemo2.Controllers
 {
     public class MemoController : ApiController
     {
         // GET api/<controller>
-        public IEnumerable<string> Get()
+        public IEnumerable<Memo> Get()
         {
-            return new string[] { "value1", "value2" };
+            //return new string[] { "value1", "value2" };
+
+            return new Memo[] { new Memo() { Id = "1", Priority = 4, Text = "Text1" }, new Memo() { Id = "2", Priority = 2, Text = "Text2" } };
         }
 
         // GET api/<controller>/5
-        public string Get(int id)
+        public Memo Get(int id)
         {
-            return "value";
+            return new Memo() {Id = "1", Priority = 4, Text = "Text1" };
         }
 
         // POST api/<controller>
