8c0fde61eab352e47401bc7f0d1f4feef6ae9d05
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-10-03 14:45:29 -0400

    Made Memo Model

---
+++ WebAPIDemo2/Models/Memo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIDemo2.Models
+{
+    /// <summary>
+    /// Memo Entity
+    /// </summary>
+    public class Memo
+    {
+        public string Id { set; get; }
+        public string Text { get; set; }
+        public decimal Priority { get; set; }
+
+        /// <summary>
+        /// This returns a summary line for the object.
+        /// </summary>
+        /// <param name="oneLine">true will print on one line online.</param>
+        /// <returns>The formatted string</returns>
+        public string PrintMemo(bool oneLine)
+        {
+            //TODO: use oneLine 
+            return String.Format("Id: {0}, Text: {1}, Priority{2}", this.Id, this.Text, this.Priority);
+        }
+    }
+}
